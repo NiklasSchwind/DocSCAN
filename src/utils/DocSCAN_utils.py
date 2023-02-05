2e3bf3622a6b7ad6f79cf996da1852d0497547a7
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-02-05 15:51:37 +0100

    Changed embedding method

--- src/utils/DocSCAN_utils.py
+++ src/utils/DocSCAN_utils.py
@@ -99,9 +99,9 @@ class DocScanDataset_BertFinetune(Dataset):
 		#lol
 		anchors = torch.tensor([i["anchor"] for i in batch]).to(torch.int)
 		print(anchors)
-		out = torch.tensor([self.texts[anchor] for anchor in anchors]).to(self.device)
+		out = [self.texts[anchor] for anchor in anchors]
 		neighbors = torch.tensor([i["anchor"] for i in batch]).to(torch.int)
-		out_2 = torch.tensor([self.texts[neighbor] for neighbor in neighbors]).to(self.device)
+		out_2 = [self.texts[neighbor] for neighbor in neighbors]
 		return {"anchor": out, "neighbor": out_2}
 
 	def collate_fn_predict(self, batch):
