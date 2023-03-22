a17086933080306f31020fcbeb8872eee99ad478
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-03-22 16:44:29 +0100

    Changed embedding method

--- src/StartExperimentsAccordingToGPUAvailability.py
+++ src/StartExperimentsAccordingToGPUAvailability.py
@@ -50,7 +50,7 @@ while count <= len(Experiments):
 			device = f'cuda:{i}'
 			now = datetime.now()
 			current_time = now.strftime("%H:%M:%S")
-			outfile = f"logs/{Experiments[count]}, started {current_time}.txt"
+			outfile = f"logs/{Experiments[count].replace(' ','_')}_started_{current_time}.txt"
 			processes[count] = mp.Process(target=start_experiment(Experiments[count], device, outfile))
 			processes[count].start()
 			freeCUDA[i] = False
