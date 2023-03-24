b950d3e19f9f7296b53140132cf9a69e449e9587
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-03-24 19:22:48 +0100

    Changed embedding method

--- src/fix_dataset.py
+++ src/fix_dataset.py
@@ -1,41 +1,28 @@
 import json
 import csv
+from os import listdir
+from os.path import isfile, join
 
 
-filename_in = 'dbpedia_csv/test.csv'
-filename_out = 'test.jsonl'
-
+#filename_in = 'dbpedia_csv/test.csv'
+filename_out = 'train.jsonl'
+split = 'test'
 
 
 def read_csv(infile):
-    rename = {0:"Company",
-                        1:"EducationalInstitution",
-                        2:"Artist",
-                        3:"Athlete",
-                        4:"OfficeHolder",
-                        5:"MeanOfTransportation",
-                        6:"Building",
-                        7:"NaturalPlace",
-                        8:"Village",
-                        9:"Animal",
-                        10:"Plant",
-                        11:"Album",
-                        12:"Film",
-                        13:"WrittenWork"}
+    rename = {0:"positiv", 1:"negativ",}
     dictlist = []
-    with open(infile, encoding="utf-8") as csv_file:  #, encoding="utf-8"
-        csv_reader = csv.reader(
-            csv_file, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True
-        )
-        for id_, row in enumerate(csv_reader):
-            label, title, description = row
-            # Original labels are [1, 2, 3, 4] ->
-            #                   ['World', 'Sports', 'Business', 'Sci/Tech']
-            # Re-map to [0, 1, 2, 3].
-            label = int(label) -1
-            text = " ".join((title, description))
-            dictlist.append({"text": text, "label": rename[int(label)]})
-    print(len(dictlist))
+    onlyfiles_neg = [f for f in listdir('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/neg') if isfile(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/neg', f))]
+    onlyfiles_pos = [f for f in listdir('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/pos') if
+                     isfile(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/pos', f))]
+
+    for f in onlyfiles_pos:
+        with open(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/pos', f), encoding='utf-8') as text:
+            dictlist.append({"text": str(text), "label": "positiv"})
+    for f in onlyfiles_pos:
+        with open(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/neg', f), encoding='utf-8') as text:
+            dictlist.append({"text": str(text), "label": "negativ"})
+
     return dictlist
 
 
@@ -47,5 +34,5 @@ def write_in_jsonl(list, outfile):
             f.write('\n')
 
 
-dictlist = read_csv(filename_in)
+dictlist = read_csv()#filename_in)
 write_in_jsonl(dictlist,filename_out)
