9aa070fd36694fc84065ba0ac911ec1ce15067ab
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-03-24 16:53:31 +0100

    Changed embedding method

--- src/fix_dataset.py
+++ src/fix_dataset.py
@@ -1,18 +1,28 @@
 import json
-
-filename = 'test.jsonl'
-
+import csv
 
 
-def read_jsonl(infile):
+filename_in = 'test.csv'
+filename_out = 'test.jsonl'
+
+
+
+def read_csv(infile):
+    rename = {0:'World', 1:'Sports', 2:'Business', 3:'Sci/Tech'}
     dictlist = []
-    rename = {'0': 'World', '1': 'Sports' ,'2': 'Business', '3': 'Science and Technology' }
-    with open(filename) as f:
-        for line in f:
-            lines = json.loads(line)
-            for lin in lines["rows"]:
-                dictlist.append({"text": lin["row"]["text"], "label": rename[str(lin["row"]["label"])]})
-            print(len(lines["rows"]))
+    with open(infile, encoding="utf-8") as csv_file:
+        csv_reader = csv.reader(
+            csv_file, quotechar='"', delimiter=",", quoting=csv.QUOTE_ALL, skipinitialspace=True
+        )
+        for id_, row in enumerate(csv_reader):
+            label, title, description = row
+            # Original labels are [1, 2, 3, 4] ->
+            #                   ['World', 'Sports', 'Business', 'Sci/Tech']
+            # Re-map to [0, 1, 2, 3].
+            label = int(label) - 1
+            text = " ".join((title, description))
+            dictlist.append({"text": text, "label": rename[int(label)]})
+    print(len(dictlist))
     return dictlist
 
 
@@ -24,5 +34,5 @@ def write_in_jsonl(list, outfile):
             f.write('\n')
 
 
-dictlist = read_jsonl(filename)
+dictlist = read_csv(filename)
 write_in_jsonl(dictlist,filename)
