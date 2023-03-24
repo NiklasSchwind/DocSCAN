e91c9f8e07d9636fae6a2c6b112792ce8a912bf2
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-03-24 19:31:23 +0100

    Changed embedding method

--- src/fix_dataset.py
+++ src/fix_dataset.py
@@ -5,23 +5,23 @@ from os.path import isfile, join
 
 
 #filename_in = 'dbpedia_csv/test.csv'
-filename_out = 'train.jsonl'
+filename_out = 'test.jsonl'
 split = 'test'
 
 
 def read_csv(infile):
     rename = {0:"positiv", 1:"negativ",}
     dictlist = []
-    onlyfiles_neg = [f for f in listdir('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/neg') if isfile(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/neg', f))]
-    onlyfiles_pos = [f for f in listdir('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/pos') if
-                     isfile(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/pos', f))]
+    onlyfiles_neg = [f for f in listdir('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/test/neg') if isfile(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/test/neg', f))]
+    onlyfiles_pos = [f for f in listdir('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/test/pos') if
+                     isfile(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/test/pos', f))]
 
     for f in onlyfiles_pos:
-        with open(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/pos', f), encoding='utf-8') as text:
+        with open(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/test/pos', f), encoding='utf-8') as text:
             lines = text.readlines()
             dictlist.append({"text": str(lines), "label": "positiv"})
     for f in onlyfiles_neg:
-        with open(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/train/neg', f), encoding='utf-8') as text:
+        with open(join('/vol/fob-vol7/mi19/schwindn/DocSCAN/IMDB/aclImdb/test/neg', f), encoding='utf-8') as text:
             lines = text.readlines()
             dictlist.append({"text": str(lines), "label": "negativ"})
 
