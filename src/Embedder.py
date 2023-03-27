ae1d1f6358e22bd2bae0c31848f28312a20e92bc
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-03-27 21:15:24 +0200

    Changed embedding method

--- src/Embedder.py
+++ src/Embedder.py
@@ -108,11 +108,11 @@ class Embedder:
                 encoded_inputs = tokenizer.encode_plus(text, padding='max_length', return_tensors='pt').to(
                 self.device)
                 input_ids.append(encoded_inputs['input_ids'][:512])
-                print(encoded_inputs['input_ids'][:512].shape)
+                print(encoded_inputs['input_ids'][0,:512].shape)
                 attention_mask.append(encoded_inputs['attention_mask'][:512])
 
-            input_ids = torch.cat(input_ids, dim=0 )
-            attention_mask = torch.cat(attention_mask,dim=0)
+            input_ids = torch.cat(input_ids, dim=1 )
+            attention_mask = torch.cat(attention_mask,dim=1)
             print(input_ids)
             '''
             print(encoded_inputs)
