2b9b831794de25fe689e165f7899876adbfb435f
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-03-30 15:25:45 +0200

    Changed embedding method

--- src/Embedder.py
+++ src/Embedder.py
@@ -189,7 +189,7 @@ class Embedder:
         # Define sentence transformer model using CLS pooling
         tokenizer = AutoTokenizer.from_pretrained("princeton-nlp/sup-simcse-roberta-large")
         model = AutoModel.from_pretrained("princeton-nlp/sup-simcse-roberta-large")
-        tokenized_texts = tokenizer.tokenizer.encode_plus(self.texts, padding=True, return_tensors='pt').to(
+        tokenized_texts = tokenizer.encode_plus(self.texts, padding=True, return_tensors='pt').to(
                 self.device)
         corpus_embeddings = model.encode(tokenized_texts)
 
