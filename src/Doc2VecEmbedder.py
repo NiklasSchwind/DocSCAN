20e97e7960bee9479dd7f3092ef0e62afaf29b3e
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-03-31 16:41:29 +0200

    Changed embedding method

--- src/Doc2VecEmbedder.py
+++ src/Doc2VecEmbedder.py
@@ -47,6 +47,7 @@ class Doc2Vec_Embedder:
 
     def TrainDoc2Vec(self):
         train = self.train['text'].apply(self.clean_text)
+        print(train)
         train_tagged = train.apply(lambda r: TaggedDocument(words=self.tokenize_text(r['text']), tags=['unwichtig']), axis=1)
         model_dbow = Doc2Vec(dm=0, vector_size=300, negative=5, hs=0, min_count=2, sample=0, workers= self.cores)
         model_dbow.build_vocab([x for x in tqdm(train_tagged.values)])
