348d01a748b8e01e79bcfecad660048a5197b158
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-04-05 16:01:02 +0200

    Changed embedding method

--- src/NLPScanModels.py
+++ src/NLPScanModels.py
@@ -60,7 +60,72 @@ class Dataset_Bert(torch.utils.data.Dataset):
 
 
 
+class Trainer_Bert:
+    def __init__(self, num_classes: int, ):
 
+        self.model = BertClassifier(number_classes=num_classes)
+
+
+
+    def finetune_BERT(self,model, train_data,  learning_rate, epochs, device):
+
+        train = Dataset_Bert(train_data)#, Dataset_Bert(val_data)
+
+        train_dataloader = torch.utils.data.DataLoader(train, batch_size=2, shuffle=True)
+        #val_dataloader = torch.utils.data.DataLoader(val, batch_size=2)
+
+
+
+        criterion = nn.CrossEntropyLoss()
+        optimizer = Adam(model.parameters(), lr=learning_rate)
+
+
+        model = model.to(device)
+        criterion = criterion.to(device)
+
+        for epoch_num in range(epochs):
+
+            total_acc_train = 0
+            total_loss_train = 0
+
+            for train_input, train_label in tqdm(train_dataloader):
+                train_label = train_label.to(device)
+                mask = train_input['attention_mask'].to(device)
+                input_id = train_input['input_ids'].squeeze(1).to(device)
+
+                output = model(input_id, mask)
+
+                batch_loss = criterion(output, train_label.long())
+                total_loss_train += batch_loss.item()
+
+                acc = (output.argmax(dim=1) == train_label).sum().item()
+                total_acc_train += acc
+
+                model.zero_grad()
+                batch_loss.backward()
+                optimizer.step()
+
+            total_acc_val = 0
+            total_loss_val = 0
+            '''
+            with torch.no_grad():
+    
+                for val_input, val_label in val_dataloader:
+                    val_label = val_label.to(device)
+                    mask = val_input['attention_mask'].to(device)
+                    input_id = val_input['input_ids'].squeeze(1).to(device)
+    
+                    output = model(input_id, mask)
+    
+                    batch_loss = criterion(output, val_label.long())
+                    total_loss_val += batch_loss.item()
+    
+                    acc = (output.argmax(dim=1) == val_label).sum().item()
+                    total_acc_val += acc
+            '''
+            print(
+                f'Epochs: {epoch_num + 1} | Train Loss: {total_loss_train / len(train_data): .3f} \
+                    | Train Accuracy: {total_acc_train / len(train_data): .3f}' )
 
 
 
