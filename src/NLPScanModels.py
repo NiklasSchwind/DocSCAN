3e167a751f2be1464a9f5a01a9a802192d273545
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-04-19 11:52:12 +0200

    Bla

--- src/NLPScanModels.py
+++ src/NLPScanModels.py
@@ -441,7 +441,7 @@ class DocSCAN_Trainer:
         dataset = list(zip(prototype_embeddings, augmented_prototype_embeddings))
 
         dataloader = torch.utils.data.DataLoader(dataset, shuffle=True, batch_size=self.batch_size)
-        dataloader.to(self.device)
+
         train_iterator = range(int(num_epochs))
 
         for epoch in train_iterator:
