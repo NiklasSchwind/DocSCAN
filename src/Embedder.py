b6816d77e3f0cc74b1df8679a48b0b3358856b44
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-05-26 20:28:14 +0200

    Bla

--- src/Embedder.py
+++ src/Embedder.py
@@ -52,16 +52,16 @@ class Embedder:
     def safe_embeddings(self,mode: Literal['test', 'train','embed']):
         if self.embedding_method == 'IndicativeSentence':
             ind = self.indicative_sentence
-            np.save(os.path.join(self.path, f"{mode}-{self.embedding_method}-embeddings_{ind.replace('<','^').replace('>','?').replace(' ','_').replace('!', '5')}.npy"), self.embeddings.cpu())
+            np.save(os.path.join(self.path, f"{mode}-{self.embedding_method}-embeddings_{ind.replace('<','^').replace('>','?').replace(' ','_').replace('!', '5').replace('.', '6')}.npy"), self.embeddings.cpu())
         else:
             np.save(os.path.join(self.path, f"{mode}-{self.embedding_method}-embeddings.npy"),
                     self.embeddings.cpu())
 
     def load_embeddings(self,mode: Literal['test', 'train','embed']):
-        print('Hello')
+
         if self.embedding_method == 'IndicativeSentence':
             ind = self.indicative_sentence
-            return torch.from_numpy(np.load(os.path.join(self.path, f"{mode}-{self.embedding_method}-embeddings_{ind.replace('^','<').replace('?','>').replace('_',' ').replace('5', '!')}.npy")))
+            return torch.from_numpy(np.load(os.path.join(self.path, f"{mode}-{self.embedding_method}-embeddings_{ind.replace('<','^').replace('>','?').replace(' ','_').replace('!', '5').replace('.', '6')}.npy")))
         else:
             return torch.from_numpy(np.load(os.path.join(self.path, f"{mode}-{self.embedding_method}-embeddings.npy")))
 
@@ -74,7 +74,7 @@ class Embedder:
 
         if self.embedding_method == 'IndicativeSentence':
             ind = self.indicative_sentence
-            suffix = f"_{ind.replace('^','<').replace('?','>').replace('_',' ').replace('5', '!')}"
+            suffix = f"_{ind.replace('<','^').replace('>','?').replace(' ','_').replace('!', '5').replace('.', '6')}"
         else:
             suffix = ''
 
@@ -84,6 +84,7 @@ class Embedder:
             embeddings = self.load_embeddings(mode)
             self.embeddings = embeddings
         else:
+
             embeddings = self.embedding_methods[self.embedding_method](texts)
             self.embeddings = embeddings
             if safeEmbeddings:
