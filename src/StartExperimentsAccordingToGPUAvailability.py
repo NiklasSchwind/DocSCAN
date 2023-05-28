78198d27b2adb38457f9b4590358605152ccde97
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-05-28 13:27:47 +0200

    Bla

--- src/StartExperimentsAccordingToGPUAvailability.py
+++ src/StartExperimentsAccordingToGPUAvailability.py
@@ -358,7 +358,7 @@ Experiments = [
 				'--indicative_sentence': 'Read <mask> for more on this topic.', '--indicative_sentence_position': 'last' }]
 '''
 Experiments = [
-{'--embedding_model': 'IndicativeSentence', '--path': '20newsgroup', '--clustering_method': 'EntropyLoss',
+{'--embedding_model': 'IndicativeSentence', '--path': 'ag_news', '--clustering_method': 'EntropyLoss',
 				'--model_method': 'DocSCAN', '--num_epochs': 5, '--repetitions': 10, '--new_embeddings': 'True',
 				'--indicative_sentence': 'News Category: <mask>. ', '--indicative_sentence_position': 'first' },]
 '''
