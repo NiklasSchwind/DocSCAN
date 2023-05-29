65b46aa014c3291a51aef6c08a78f48cd41d62ee
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-05-29 12:29:58 +0200

    Bla

--- src/ReadAndVisualiseExperimentOutcomes.py
+++ src/ReadAndVisualiseExperimentOutcomes.py
@@ -147,7 +147,7 @@ def display_experiments(mode: Literal['ratio', 'entropy'], mypath):
 
         frame = frame[frame.Accuracy != 'Experiment not finished'].sort_values('Entropy Weight')
         frame = frame[frame.Dataset == '20newsgroup'].sort_values('Entropy Weight')
-        frame = frame[frame['Clustering Method'] == 'SCANLoss'].sort_values('Entropy Weight')
+        frame = frame[frame['Clustering Method'] == 'EntropyLoss'].sort_values('Entropy Weight')
 
     with pd.option_context('display.max_rows', None,
                            'display.max_columns', None,
