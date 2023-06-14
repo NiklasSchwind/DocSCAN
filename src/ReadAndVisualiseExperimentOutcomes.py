9f9e0234ca5d6d6d25bfaa31c53b273e1f68b01c
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-06-14 10:44:10 +0200

    Bla

--- src/ReadAndVisualiseExperimentOutcomes.py
+++ src/ReadAndVisualiseExperimentOutcomes.py
@@ -148,7 +148,7 @@ def display_experiments(mode: Literal['ratio', 'entropy'], mypath):
         frame = return_list_of_accuracies_entropy_weight(mypath)
 
         frame = frame[frame.Accuracy != 'Experiment not finished'].sort_values('Entropy Weight')
-        frame = frame[frame.Dataset == 'IMDB'].sort_values('Entropy Weight')
+        frame = frame[frame.Dataset == 'ag'].sort_values('Entropy Weight')
         #frame = frame[frame['Indicative Sentences'] == 'Category:'].sort_values('Entropy Weight')
         frame = frame[frame['Clustering Method'] == 'EntropyLoss'].sort_values('Entropy Weight')
 
