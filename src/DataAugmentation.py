f1ce98c4d1bbc1704ef50b6b0c5b32d64b11c820
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-06-19 16:16:09 +0200

    Bla

--- src/DataAugmentation.py
+++ src/DataAugmentation.py
@@ -24,7 +24,7 @@ class DataAugmentation:
             augmented_data = []
             tokenizer = MarianTokenizer.from_pretrained(f'Helsinki-NLP/opus-mt-{language_before}-{language}')
             model = MarianMTModel.from_pretrained(f'Helsinki-NLP/opus-mt-{language_before}-{language}').to(self.device)
-            for i in tqdm(range(len(data))):
+            for i in tqdm(range(0,len(data),self.batch_size)):
                 batch = data[i:min(i+self.batch_size,len(data))]
                 augmented_data += self._translate_texts(tokenizer,model,language, batch)
             data = augmented_data
