2565cb83e626b6ff163ef8095f3ca4f2956098ef
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-06-26 18:40:04 +0200

    Bla

--- src/NeighborDataset.py
+++ src/NeighborDataset.py
@@ -54,10 +54,10 @@ class Neighbor_Dataset:
     def create_neighbor_dataset(self, data, createNewDataset = False):
 
         if os.path.exists(os.path.join(self.path, f"neighbor_dataset_{self.embedding_method}.csv")) and self.num_neighbors == 5 and not createNewDataset and self.args.embedding_model != 'IndicativeSentence':
-            print("loading neighbor dataset")
+            print("loading SBERT neighbor dataset")
             neighbor_dataset = pd.read_csv(os.path.join(self.path, f"neighbor_dataset_{self.embedding_method}.csv"))
         elif os.path.exists(os.path.join(self.path, f"neighbor_dataset_{self.embedding_method}_indicativesentence_{self.args.indicative_sentence.replace('<','^').replace('>','?').replace(' ','_').replace('!', '5').replace('.', '6')}.csv")) and self.num_neighbors == 5 and not createNewDataset and self.args.embedding_model == 'IndicativeSentence':
-            print("loading neighbor dataset")
+            print("loading IS neighbor dataset")
             neighbor_dataset = pd.read_csv(os.path.join(self.path, f"neighbor_dataset_{self.embedding_method}_indicativesentence_{self.args.indicative_sentence.replace('<','^').replace('>','?').replace(' ','_').replace('!', '5').replace('.', '6')}.csv"))
 
         elif os.path.exists(os.path.join(self.path, f"neighbor_dataset_{self.embedding_method}" + str(self.num_neighbors) + ".csv")) and not createNewDataset:
