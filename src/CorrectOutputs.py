c08a0820e149a6943a3c0e358f978c103050b521
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-07-13 17:16:47 +0200

    Bla

--- src/CorrectOutputs.py
+++ src/CorrectOutputs.py
@@ -30,6 +30,12 @@ def correct_accuracy_calculation_selflabeling(in_file, out_file):
 
 def correct_folder(inpath, outpath):
     onlyfiles = [str(f) for f in listdir(inpath) if isfile(join(inpath, f))]
+    for file in onlyfiles:
+        in_file = '/'.join([inpath,file])
+        out_file = '/'.join([outpath, file])
+        print(in_file)
+        print(out_file)
+        #correct_accuracy_calculation_selflabeling(in_file, out_file)
     print(onlyfiles)
 
 #correct_accuracy_calculation_selflabeling('/vol/fob-vol7/mi19/schwindn/DocSCAN/DeletionRatioExperimentsNew/Dataset_TREC-6_Em_IS_clustering_method_EntropyLoss_model_method_DocSCAN_finetuning_multi_epochs_5_indicativesentence__^mask?._entropy_weight_3.0_threshold_0.99_augmentation_method_Deletion_ratio_0_new.txt')
