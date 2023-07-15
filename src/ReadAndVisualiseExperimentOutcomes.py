5e1f116a2f2b5023f21b31069a1110db888d7bf6
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-07-15 16:01:11 +0200

    Bla

--- src/ReadAndVisualiseExperimentOutcomes.py
+++ src/ReadAndVisualiseExperimentOutcomes.py
@@ -64,7 +64,7 @@ def return_list_of_accuracies_selflabeling(path):
                         return_next_in_list('clustering', file.name.split('_'),2),
                         return_next_in_list('epochs', file.name.split('_'), 1),
                         return_next_in_list('threshold', file.name.split('_'), 1),
-                        return_next_in_list('entropy', file.name.split('_'), 2) if 'Em_IS' in str(file) else 0,
+                        return_next_in_list('entropy', file.name.split('_'), 2) if 'Em_IS' in str(file) else return_next_in_list('weight', file.name.split('_'), 1),
                         #return_next_in_list('threshold', file.name.split('_'), 6),
                         before_selflabeling,
                         after_selflabeling,
