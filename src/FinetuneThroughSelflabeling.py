773b2fac539312707aa9d064b6f12e6a83407a2d
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-07-15 12:42:14 +0200

    Bla

--- src/FinetuneThroughSelflabeling.py
+++ src/FinetuneThroughSelflabeling.py
@@ -160,6 +160,7 @@ class FinetuningThroughSelflabeling:
         self.train_data["label"] = targets_train
         self.train_data["clusters"] = docscan_clusters_train
         self.train_data["probabilities"] = probabilities_train
+        print(len(self.train_data["probabilities"]))
         prototype_indexes = self.train_data.loc[self.train_data["probabilities"].apply(softmax).apply(np.max) >= self.threshold].index
         print(prototype_indexes)
         return prototype_indexes
