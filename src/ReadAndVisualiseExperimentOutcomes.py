7e36cae8add753d524219224404125e4f8d8685f
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-07-19 11:29:26 +0200

    Bla

--- src/ReadAndVisualiseExperimentOutcomes.py
+++ src/ReadAndVisualiseExperimentOutcomes.py
@@ -138,7 +138,7 @@ def display_selflabeling_experiments():
         print(frame)
 
 def display_selflabeling_experiments_list_of_accuracy():
-    mypath = '/vol/fob-vol7/mi19/schwindn/DocSCAN/DeletionRatioExperimentsCorrected'
+    mypath = '/vol/fob-vol7/mi19/schwindn/DocSCAN/CroppingExperimentsCorrected'
     frame = return_list_of_accuracies_selflabeling(mypath)
 
     frame = frame[frame.Difference != 'Experiment not finished'].sort_values(
