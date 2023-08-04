2ae08f0cc54f41aebf4954ff9cdc5864192c98aa
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-08-04 19:08:23 +0200

    Commit

--- src/StartExperimentsAccordingToGPUAvailability.py
+++ src/StartExperimentsAccordingToGPUAvailability.py
@@ -190,7 +190,7 @@ else:
 	count = len(Experiments) + 1
 
 processes = {}
-possible_devices = [0,1,2]#list(range(deviceCount))
+possible_devices = [2]#list(range(deviceCount))
 used_devices = []
 process_device = {}
 
