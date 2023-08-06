062c709478ae0d5e1c01b924996b6d3102cb04ff
Niklas Schwind <niklas.schwind@climateanalytics.org>
2023-08-06 14:10:39 +0200

    Commit

--- src/CorrectOutputs.py
+++ src/CorrectOutputs.py
@@ -12,27 +12,25 @@ def correct_accuracy_calculation_selflabeling(in_file, out_file=None):
     accuracies_beforeSL = []
     weighted_accuracy_afterSL = []
     accuracies_afterSL = []
-    #out = open(out_file, 'w')
+    out = open(out_file, 'w')
     for j, line in enumerate(lines_file):
         if line[0:3] == '[0.':
             accuracies_beforeSL.append(float(line.split(', ')[0][1:]))
             accuracies_afterSL.append(float(line.split(', ')[-1][:-2]))
             weighted_accuracy_afterSL.append(float(lines_file[j-11].split(', ')[0].split(' ')[3]))
-            #out.write(line)
+            out.write(line)
         elif line[0:16] == 'Macro Average F1' and i == 1:
-            print(weighted_accuracy_afterSL)
-            print(line)
-            # out.write(f'Macro Average F1: {np.mean(weighted_accuracy_afterSL).round(3)} ({np.std(weighted_accuracy_afterSL).round(3)}) \n')
+            out.write(f'Macro Average F1: {np.mean(weighted_accuracy_afterSL).round(3)} ({np.std(weighted_accuracy_afterSL).round(3)}) \n')
         elif line[0:9] == 'Accuracy:' and i == 0 and line.split(' ')[2][0] == '(':
-            #out.write(f'Accuracy: {np.mean(accuracies_beforeSL).round(3)} ({np.std(accuracies_beforeSL).round(3)}) \n')
+            out.write(f'Accuracy: {np.mean(accuracies_beforeSL).round(3)} ({np.std(accuracies_beforeSL).round(3)}) \n')
             i += 1
         elif line[0:9] == 'Accuracy:' and i == 1 and line.split(' ')[2][0] == '(':
-            #out.write(f'Accuracy: {np.mean(accuracies_afterSL).round(3)} ({np.std(accuracies_afterSL).round(3)})\n')
+            out.write(f'Accuracy: {np.mean(accuracies_afterSL).round(3)} ({np.std(accuracies_afterSL).round(3)})\n')
             i += 1
         else:
             pass
-            #out.write(line)
-    #out.close()
+            out.write(line)
+    out.close()
     file.close()
 
 def correct_folder(inpath, outpath):
@@ -45,6 +43,6 @@ def correct_folder(inpath, outpath):
         correct_accuracy_calculation_selflabeling(in_file, out_file)
 
 
-correct_accuracy_calculation_selflabeling('/vol/fob-vol7/mi19/schwindn/DocSCAN/DeletionRatioExperimentsNew/Dataset_TREC-6_Em_IS_clustering_method_EntropyLoss_model_method_DocSCAN_finetuning_multi_epochs_5_indicativesentence__^mask?._entropy_weight_3.0_threshold_0.99_augmentation_method_Deletion_ratio_0_new.txt')
+#correct_accuracy_calculation_selflabeling('/vol/fob-vol7/mi19/schwindn/DocSCAN/DeletionRatioExperimentsNew/Dataset_TREC-6_Em_IS_clustering_method_EntropyLoss_model_method_DocSCAN_finetuning_multi_epochs_5_indicativesentence__^mask?._entropy_weight_3.0_threshold_0.99_augmentation_method_Deletion_ratio_0_new.txt')
 
-#correct_folder('/vol/fob-vol7/mi19/schwindn/DocSCAN/CroppingExperiments', '/vol/fob-vol7/mi19/schwindn/DocSCAN/CroppingExperimentsDoubleCorrected')
+correct_folder('/vol/fob-vol7/mi19/schwindn/DocSCAN/DeletionRatioExperimentsNew', '/vol/fob-vol7/mi19/schwindn/DocSCAN/DeletionRatioExperimentsTwiceCorrected')
